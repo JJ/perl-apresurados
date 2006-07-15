d4ceac7cd61139a34751862b569a2a6632996b93
jmerelo <jmerelo@aa3d5b26-224e-22b5-6ce4-cdda380990a3>
2006-07-15 07:56:24 +0000

    Aniadidos comentarios

--- code/hola-y-duerme.pl
+++ code/hola-y-duerme.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 
 print "A estas horas, el Estudiante, no creyendo su buen suceso y
-deshollinando con el vestido y los ojos el zaquizamí...\n"; 
-sleep 10;
+deshollinando con el vestido y los ojos el zaquizamí...\n";
+sleep 10; # Quieto parao ahí
