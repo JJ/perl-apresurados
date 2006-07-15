dfa59df6a83be226877a4420edb570aa3a863935
jmerelo <jmerelo@aa3d5b26-224e-22b5-6ce4-cdda380990a3>
2006-07-15 12:07:18 +0000

    Algunos arreglos sobre los hashes y matrices

---
+++ code/cambiando.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Slurp;
+
+my %roles = (
+	     Madrid => 'el foro',
+	     'Cleof[áa]s' => 'El prota',
+	     '[Ee]l [Dd]iablo [Cc]ojuelo' => 'Su Satánica Majestad' ,
+	    );
+
+my $fichero_a_procesar = shift 
+  || die "Uso: $0 <nombre de fichero>n";
+
+my $texto=read_file($fichero_a_procesar);
+
+for ( keys %roles ) {
+  $texto =~ s/$_/$roles{$_}/ge;
+}
+
+print $texto;
